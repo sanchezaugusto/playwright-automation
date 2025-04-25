cbf293b30fe6612fe9fe6fe7ece8b8fb09c9558d
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-04-25 09:10:51 -0300

    login with saved cookies test was added to loginCookies

--- tests/loginCookies.spec.ts
+++ tests/loginCookies.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { InventoryPage } from '../pages/InventoryPage';
 import { testData } from '../utils/testData';
 import * as fs from 'fs';
 
@@ -9,18 +10,27 @@ test.describe('Login and Save Cookies', () => {
 
         await loginPage.goto();
         await loginPage.login(testData.validUser.email, testData.validUser.password);
-
         await page.waitForLoadState('networkidle');
 
         const cookies = await context.cookies();
-
         fs.writeFileSync('cookies.json', JSON.stringify(cookies, null, 2));
-
-        console.log('Cookies guardadas en cookies.json');
+        console.log('Cookies were saved to cookies.json');
 
         const sessionCookie = cookies.find(cookie => cookie.name === 'session-username');
         expect(sessionCookie).toBeDefined();
         expect(sessionCookie?.value).toBe('standard_user');
         expect(sessionCookie?.domain).toBe('www.saucedemo.com');
+    });
+
+    test('should login with saved cookies', async ({ page, context }) => {
+        const inventoryPage = new InventoryPage(page);
+        const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf-8'));
+
+        await context.addCookies(cookies);
+        await inventoryPage.goto();
+
+        const sessionCookie = cookies.find(cookie => cookie.name === 'session-username');
+        expect(sessionCookie).toBeDefined();
+        expect(sessionCookie?.value).toBe('standard_user');
     });
 });
