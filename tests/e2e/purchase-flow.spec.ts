baa57629c7de80a0dbdc6b08ab136a46619cd729
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-06-01 18:36:58 -0300

    purchase-flow test was reformed

--- tests/e2e/purchase-flow.spec.ts
+++ tests/e2e/purchase-flow.spec.ts
@@ -1,14 +1,38 @@
 import {test, expect} from '@playwright/test';
+import {LoginPage} from '../../pages/LoginPage';
+import {InventoryPage} from '../../pages/InventoryPage';
+import {CartPage} from '../../pages/CartPage';
+import {CheckoutOnePage} from '../../pages/CheckoutOnePage';
+import {CheckoutTwoPage} from '../../pages/CheckoutTwoPage';
+import {CheckoutCompletePage} from '../../pages/CheckoutCompletePage';
 
 test.describe('Purchase Flow', () => {
     test('should complete the purchase flow', async ({page}) => {
-        await page.goto('https://www.saucedemo.com/');
-        await page.locator('[data-test="username"]').fill('standard_user');
-        await page.locator('[data-test="password"]').fill('secret_sauce');
-        await page.locator('[data-test="login-button"]').click();
-        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-        await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-        await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
-        await page.locator('[data-test="cart"]').click();
+
+        const loginPage = new LoginPage(page);
+        const inventoryPage = new InventoryPage(page);
+        const cartPage = new CartPage(page);
+        const checkoutOnePage = new CheckoutOnePage(page);
+        const checkoutTwoPage = new CheckoutTwoPage(page);
+        const checkoutCompletePage = new CheckoutCompletePage(page);
+        
+        await loginPage.goto();
+        await loginPage.login('standard_user', 'secret_sauce');
+
+        await inventoryPage.goto();
+        await inventoryPage.addProductToCart('#add-to-cart-sauce-labs-backpack');
+        await inventoryPage.addProductToCart('#add-to-cart-sauce-labs-bolt-t-shirt');
+        await inventoryPage.addProductToCart('#add-to-cart-sauce-labs-fleece-jacket');
+        await inventoryPage.gotoCart();
+
+        await cartPage.clickCheckout();
+
+        await checkoutOnePage.fillCheckoutForm('John', 'Doe', '12345');
+        await checkoutTwoPage.clickFinish();
+
+        await expect(checkoutCompletePage.orderMessage).toHaveText('Thank you for your order!');
+
+        await checkoutCompletePage.clickBackHome();
+        await inventoryPage.logout();
     });
 });
