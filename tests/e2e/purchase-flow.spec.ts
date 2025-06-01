2719880677f9789a35d60e5ad5f164407c75dcaa
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-06-01 16:56:57 -0300

    purchase-flow first steps

---
+++ tests/e2e/purchase-flow.spec.ts
@@ -0,0 +1,14 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Purchase Flow', () => {
+    test('should complete the purchase flow', async ({page}) => {
+        await page.goto('https://www.saucedemo.com/');
+        await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+        await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+        await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+        await page.locator('[data-test="cart"]').click();
+    });
+});
