933b2d44f30f5c2d16ac34e50d0fd508891206df
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-06-02 19:12:17 -0300

    loginVisualTest is skipped

--- tests/loginVisualTest.spec.ts
+++ tests/loginVisualTest.spec.ts
@@ -1,11 +1,11 @@
 import {test, expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
 
-test.describe('Login Visual Test', () => {
+test.describe.skip('Login Visual Test', () => {
     test('should match snapshot', async ({ page }) => {
         const loginPage = new LoginPage(page);
 
         await loginPage.goto();
-        expect( await loginPage.page.screenshot()).toMatchSnapshot();
+        expect( await loginPage.page.screenshot()).toMatchSnapshot('login-page.png');
     });
 });
