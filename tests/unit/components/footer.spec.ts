54d327653945e20f4087418bc7a6956a8fc687ce
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-06-05 21:34:32 -0300

    footer test was refactored

--- tests/unit/components/footer.spec.ts
+++ tests/unit/components/footer.spec.ts
@@ -1,15 +1,17 @@
 import {test, expect} from '@playwright/test';
 import {InventoryPage} from '../../../pages/InventoryPage';
+import {loadCookiesOrLogin} from '../../../utils/loadCookiesOrLogin';
 
 test.describe('Footer', () => {
     let inventoryPage: InventoryPage;
 
-    test.beforeEach(async ({ page }) => {
-        footer = new Footer(page);
-        await footer.goto();
+    test.beforeEach(async ({ page, context }) => {
+        await loadCookiesOrLogin(page , context);
+        inventoryPage = new InventoryPage(page);
+        await inventoryPage.goto();
     });
 
     test('should display footer', async () => {
-        await expect(footer.footer).toBeVisible();
+        await expect(inventoryPage.twitterIcon).toHaveAttribute('href','https://twitter.com/saucelabs');
     });
 });
