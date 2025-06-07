2a6c22a37b9f399704da5e092315acd40b2f6209
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-06-06 22:37:10 -0300

    tests for checking facebook and linkedin link were added

--- tests/unit/components/footer.spec.ts
+++ tests/unit/components/footer.spec.ts
@@ -11,7 +11,15 @@ test.describe('Footer', () => {
         await inventoryPage.goto();
     });
 
-    test('should display footer', async () => {
+    test('should display twitter link', async () => {
         await expect(inventoryPage.twitterIcon).toHaveAttribute('href','https://twitter.com/saucelabs');
     });
+
+    test('should display facebook link', async () => {
+        await expect(inventoryPage.facebookIcon).toHaveAttribute('href','https://www.facebook.com/saucelabs');
+    })
+
+    test('should display linkedin link', async () => {
+        await expect(inventoryPage.linkedinIcon).toHaveAttribute('href','https://www.linkedin.com/company/sauce-labs/');
+    })
 });
